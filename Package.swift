498bbb0cdd3aa4a27d07bb0f539185421efeea2b
Jay Sandhu <jaysandhu1993@gmail.com>
2025-06-24 17:53:11 +0100

    Version 5.1.1

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PSReaderBinary",
-            url: "https://pagesuite-builds.s3.eu-west-1.amazonaws.com/spm/psreader/5.1.0/PSReader.xcframework.zip",
-            checksum: "1bb529d06298e348a8307fc38063d6438f1632aabe247696f80ce2684a2ab19c"
+            url: "https://pagesuite-builds.s3.eu-west-1.amazonaws.com/spm/psreader/5.1.1/PSReader.xcframework.zip",
+            checksum: "2d5f622d7e67be45edc3820c6a2e7767fae2158854a81e3f3c08f1d5071a92a2"
         ),
         .binaryTarget(
             name: "PSReaderUIBinary",
-            url: "https://pagesuite-builds.s3.eu-west-1.amazonaws.com/spm/psreader-ui/5.1.0/PSReaderUI.xcframework.zip",
-            checksum: "3e47bca94349d02e9b4a70cc41ec6fde282bcb2bbc02573db7e858104742791d"
+            url: "https://pagesuite-builds.s3.eu-west-1.amazonaws.com/spm/psreader-ui/5.1.1/PSReaderUI.xcframework.zip",
+            checksum: "7d1bd745d953cbde3ca0dc921e48dc8a834d38ab24db0a0aebf0d7f7f44113c6"
         ),
         .target(
             name: "PSReaderSDK",
